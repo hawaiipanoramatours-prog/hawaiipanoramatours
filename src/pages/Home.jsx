6d747514c08fde140441ee8567960a9f23565b29
hawaiipanoramatours-prog <hawaiipanoramatours@gmail.com>
2025-12-11 16:58:46 -1000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,30 @@
 import { useOutletContext } from 'react-router-dom'
-import { Hero, HowItWorks, ServicesSection, Offerings, Testimonials, ContactCTA } from '../components/Sections'
+import {
+  Hero,
+  HowItWorks,
+  ServicesSection,
+  Offerings,
+  Gallery,
+  Testimonials,
+  ContactCTA,
+} from '../components/Sections.impl'
 
-export default function Home(){
+export default function Home() {
   const { content } = useOutletContext()
   const lang = content.i18n?.default || 'de'
+
   return (
-    <div className="min-h-screen">
+    <>
       <Hero content={content} lang={lang} />
       <HowItWorks content={content} lang={lang} />
       <ServicesSection content={content} lang={lang} />
       <Offerings content={content} lang={lang} />
+
+      {/* 👇 NEU: Deine Bilder-Galerie zwischen Offers & Testimonials */}
+      <Gallery />
+
       <Testimonials content={content} />
       <ContactCTA content={content} lang={lang} />
-    </div>
+    </>
   )
 }
