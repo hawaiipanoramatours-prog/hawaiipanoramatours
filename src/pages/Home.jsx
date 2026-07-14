10642af8d82dee3a4d18e6d8caee82d015f7c44a
hawaiipanoramatours-prog <hawaiipanoramatours@gmail.com>
2026-07-13 16:54:39 -1000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,21 +7,20 @@ import {
   Gallery,
   Testimonials,
   ContactCTA,
+  MalamaAina,
 } from '../components/Sections.impl'
-
 export default function Home() {
   const { content, lang } = useOutletContext()
-
   return (
     <>
       <Hero content={content} lang={lang} />
       {/* <HowItWorks content={content} lang={lang} /> */}
       <Offerings content={content} lang={lang} />
-
-     <Gallery lang={lang} />
-     <Testimonials content={content} lang={lang} />
-
+      <Gallery lang={lang} />
+      <Testimonials content={content} lang={lang} />
+      <MalamaAina lang={lang} />
       <ContactCTA content={content} lang={lang} />
     </>
   )
 }
+
